640d455c44629a00556344fc4510b1b937d91403
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2022-03-17 14:24:56 +0530

    added navbar component and set the links in navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from "react";
 import "./App.css";
-import Mockman from "mockman-js";
+import Navbar from "./components/shared/NavBar/NavBar";
+import Home from "./pages/home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
-      
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/shared/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./navbar.css";
+import { BsHeartFill, BsCartFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+
+    return (
+        <div className="">
+            <nav className="navigation">
+                <div className="nav-left">
+                    <Link to="/" className="txt-l fw-black p-s m-s">Crypto&nbsp;Store</Link>
+                </div>
+
+                <div className="relative">
+                    <input type="search" name="searchbar" className="p-s m-s input-box br-s" placeholder="Search items here" />
+                    <span className="input-icon">
+                        <FaSearch size={22} />
+                    </span>
+                </div>
+
+                <div className="nav-right">
+                    <button className="btn btn-link m-m">
+                        <Link to="/login">Login</Link>
+                    </button>
+
+                    <Link to="/wishlist" className="txt-m fw-black p-s m-s">
+                        <div className="badge">
+                            <BsHeartFill className="icons" />
+                            <div className="number-badge txt-s">10</div>
+                        </div>
+                    </Link>
+
+                    <Link to="/cart" className="txt-m fw-black p-s m-s">
+                        <div className="badge">
+                            <BsCartFill className="icons" />
+                            <div className="number-badge txt-s">10</div>
+                        </div>
+                    </Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
