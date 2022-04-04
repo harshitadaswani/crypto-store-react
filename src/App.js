14e2c08e86522745eddf96d9ffe171b7e26ae588
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2022-04-05 01:41:59 +0530

    refactor: new lines

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Navbar, Footer } from "./components";
 import { Home, Products, Cart } from "./pages";
 import AppRouter from "./routes/AppRoutes";
 
-
 function App() {
   return (
     <div className="App">
